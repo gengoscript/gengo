0cfa4cfec23600b800c013ee8ab2661caae94c94
Marvin the Paranoid Android <marvin.paradroid@linckii.org>
2026-06-10 08:31:29 +0200

    test: add compiler bytecode output tests (closes #86)

---
+++ src/compiler_test.zig
@@ -0,0 +1,270 @@
+const std = @import("std");
+const chunk = @import("lang/chunk.zig");
+const heap = @import("runtime/heap.zig");
+const globals = @import("lang/globals.zig");
+const Compiler = @import("lang/compiler.zig").Compiler;
+const Op = @import("lang/op.zig").Op;
+const Runtime = @import("runtime/runtime.zig").Runtime;
+const vms = @import("lang/vm_state.zig");
+const cfg = @import("runtime/config.zig");
+
+fn setup() Runtime {
+    var rt: Runtime = .{};
+    rt.initWithConfig(.{}, heap.HeapSize, heap.MaxObjects, vms.MaxStack, vms.MaxFrames, cfg.max_defers, std.testing.allocator);
+    return rt;
+}
+
+fn compile(rt: *Runtime, src: []const u8) !void {
+    chunk.setActive(&rt.chunk_state);
+    globals.setActive(&rt.globals_state);
+    heap.setActive(&rt.heap_state);
+    chunk.reset();
+    globals.reset();
+    heap.reset();
+
+    var compiler = Compiler.init(src, .{});
+    try compiler.compile(true);
+}
+
+test "compiler: empty source emits halt" {
+    var rt = setup();
+    defer rt.deinit();
+    try compile(&rt, "");
+    const c = &rt.chunk_state;
+    try std.testing.expectEqual(@as(usize, 1), c.code_len);
+    try std.testing.expectEqual(@intFromEnum(Op.halt), c.code[0]);
+}
+
+test "compiler: integer constant 42" {
+    var rt = setup();
+    defer rt.deinit();
+    try compile(&rt,
+        \\func f() int { return 42 }
+    );
+    const c = &rt.chunk_state;
+
+    try std.testing.expectEqual(@as(usize, 15), c.code_len);
+
+    try std.testing.expect(c.consts[0] == .int);
+
+    try std.testing.expectEqual(@intFromEnum(Op.ret_const), c.code[3]);
+
+    try std.testing.expectEqual(@intFromEnum(Op.make_closure), c.code[8]);
+
+    try std.testing.expectEqual(@intFromEnum(Op.def_global), c.code[11]);
+
+    try std.testing.expectEqual(@intFromEnum(Op.halt), c.code[14]);
+}
+
+test "compiler: var global int" {
+    var rt = setup();
+    defer rt.deinit();
+    try compile(&rt, "var x = 42");
+
+    const c = &rt.chunk_state;
+
+    try std.testing.expectEqual(@as(usize, 7), c.code_len);
+
+    try std.testing.expectEqual(@intFromEnum(Op.constant), c.code[0]);
+    try std.testing.expectEqual(@intFromEnum(Op.def_global), c.code[3]);
+    try std.testing.expectEqual(@intFromEnum(Op.halt), c.code[6]);
+}
+
+test "compiler: const_add fusion" {
+    var rt = setup();
+    defer rt.deinit();
+    try compile(&rt,
+        \\func f() int { return 1 + 2 }
+    );
+    const c = &rt.chunk_state;
+
+    try std.testing.expect(c.consts[0] == .int);
+    try std.testing.expect(c.consts[1] == .int);
+
+    var found_const_add = false;
+    var i: usize = 0;
+    while (i < c.code_len) : (i += 1) {
+        if (c.code[i] == @intFromEnum(Op.const_add)) {
+            found_const_add = true;
+            break;
+        }
+    }
+    try std.testing.expect(found_const_add);
+}
+
+test "compiler: ret_const peephole" {
+    var rt = setup();
+    defer rt.deinit();
+    try compile(&rt,
+        \\func f() int { return 42 }
+    );
+    const c = &rt.chunk_state;
+
+    var found_ret_const = false;
+    var i: usize = 0;
+    while (i < c.code_len) : (i += 1) {
+        if (c.code[i] == @intFromEnum(Op.ret_const)) {
+            found_ret_const = true;
+            break;
+        }
+    }
+    try std.testing.expect(found_ret_const);
+}
+
+test "compiler: def_global with string constant" {
+    var rt = setup();
+    defer rt.deinit();
+    try compile(&rt,
+        \\func f() int { return 42 }
+    );
+
+    const c = &rt.chunk_state;
+    var found_name = false;
+    var i: usize = 0;
+    while (i < c.const_count) : (i += 1) {
+        if (c.consts[i] == .string and std.mem.eql(u8, c.consts[i].string, "f")) {
+            found_name = true;
+            break;
+        }
+    }
+    try std.testing.expect(found_name);
+}
+
+test "compiler: make_closure emitted" {
+    var rt = setup();
+    defer rt.deinit();
+    try compile(&rt,
+        \\func f() int { return 42 }
+    );
+    const c = &rt.chunk_state;
+    var found_make_closure = false;
+    var j: usize = 0;
+    while (j < c.code_len) : (j += 1) {
+        if (c.code[j] == @intFromEnum(Op.make_closure)) {
+            found_make_closure = true;
+            break;
+        }
+    }
+    try std.testing.expect(found_make_closure);
+}
+
+test "compiler: closure captures upvalue" {
+    var rt = setup();
+    defer rt.deinit();
+    try compile(&rt,
+        \\func makeCounter() func() int {
+        \\    var x = 0
+        \\    return func() int { x = x + 1; return x }
+        \\}
+    );
+    _ = &rt;
+
+    var found_get_upvalue = false;
+    var found_set_upvalue = false;
+    const c = &rt.chunk_state;
+    var i: usize = 0;
+    while (i < c.code_len) : (i += 1) {
+        if (c.code[i] == @intFromEnum(Op.get_upvalue)) found_get_upvalue = true;
+        if (c.code[i] == @intFromEnum(Op.set_upvalue)) found_set_upvalue = true;
+    }
+    try std.testing.expect(found_get_upvalue);
+    try std.testing.expect(found_set_upvalue);
+}
+
+test "compiler: struct field access" {
+    var rt = setup();
+    defer rt.deinit();
+    try compile(&rt,
+        \\type Point struct { x int, y int }
+        \\
+        \\func getX(p Point) int { return p.x }
+    );
+    const c = &rt.chunk_state;
+
+    var found_get_field = false;
+    var i: usize = 0;
+    while (i < c.code_len) : (i += 1) {
+        if (c.code[i] == @intFromEnum(Op.get_field)) {
+            found_get_field = true;
+            break;
+        }
+    }
+    try std.testing.expect(found_get_field);
+}
+
+test "compiler: named return values" {
+    var rt = setup();
+    defer rt.deinit();
+    try compile(&rt,
+        \\func div(a int, b int) (result int, err error) {
+        \\    if b == 0 { return 0, "division by zero" }
+        \\    result = a / b
+        \\    return
+        \\}
+    );
+    _ = &rt;
+
+    const c = &rt.chunk_state;
+    try std.testing.expect(c.code_len > 0);
+    try std.testing.expectEqual(@intFromEnum(Op.halt), c.code[c.code_len - 1]);
+}
+
+test "compiler: multi-value return" {
+    var rt = setup();
+    defer rt.deinit();
+    try compile(&rt,
+        \\func pair() (int, int) { return 1, 2 }
+    );
+    const c = &rt.chunk_state;
+
+    var found_build_tuple = false;
+    var i: usize = 0;
+    while (i < c.code_len) : (i += 1) {
+        if (c.code[i] == @intFromEnum(Op.build_tuple)) found_build_tuple = true;
+    }
+    try std.testing.expect(found_build_tuple);
+}
+
+test "compiler: for-in loop bytecode" {
+    var rt = setup();
+    defer rt.deinit();
+    try compile(&rt,
+        \\func sum(a []int) int {
+        \\    var s = 0
+        \\    for x in a { s = s + x }
+        \\    return s
+        \\}
+    );
+    const c = &rt.chunk_state;
+
+    var found_iter_init = false;
+    var i: usize = 0;
+    while (i < c.code_len) : (i += 1) {
+        if (c.code[i] == @intFromEnum(Op.iter_init)) {
+            found_iter_init = true;
+            break;
+        }
+    }
+    try std.testing.expect(found_iter_init);
+}
+
+test "compiler: nested function has correct ip" {
+    var rt = setup();
+    defer rt.deinit();
+    try compile(&rt,
+        \\func outer() int {
+        \\    func inner() int { return 99 }
+        \\    return inner()
+        \\}
+    );
+    _ = &rt;
+
+    const c = &rt.chunk_state;
+    var i: usize = 0;
+    while (i < c.const_count) : (i += 1) {
+        if (c.consts[i] == .object and c.consts[i].object.* == .function) {
+            const func = c.consts[i].object.*.function;
+            try std.testing.expect(func.ip < c.code_len);
+        }
+    }
+}
